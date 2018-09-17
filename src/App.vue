9267ecc4a3570efb70026f8ab955edda2a4ccec6
XPC11059 <1105912320@qq.com>
2018-09-17 22:33:50 +0800

    day 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,63 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!--<div id="nav">-->
+      <!--<router-link to="/">Home</router-link> |-->
+      <!--&lt;!&ndash;<router-link to="/about">About</router-link>&ndash;&gt;-->
+    <!--</div>-->
+    <el-container>
+      <el-aside width="200px">
+        <el-row>
+          <el-col :span="24">
+            <el-menu
+                    default-active="2"
+                    class="el-menu-vertical-demo"
+                    @open="handleOpen"
+                    @close="handleClose"
+                    background-color="#5C8CFF"
+                    text-color="#fff"
+                    active-text-color="#FAFF52">
+              <el-submenu index="1">
+                <template slot="title">
+                  <!--<i class="el-icon-location"></i>-->
+                  <img src="./assets/index.png" />
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -16,16 +69,43 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
+div, body, html{
+  padding: 0;
+  margin: 0;
+  height: 100%;
+}
+.el-container{
+}
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  /*line-height: 60px;*/
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  /*line-height: 160px;*/
 }
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+/*.el-container:nth-child(5) .el-aside,*/
+/*.el-container:nth-child(6) .el-aside {*/
+  /*line-height: 260px;*/
+/*}*/
+
+/*.el-container:nth-child(7) .el-aside {*/
+  /*line-height: 320px;*/
+/*}*/
 </style>
